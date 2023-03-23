8e2e4ead92b7060821ab870d00b49af66d582db4
XhinoKurtaj <xhino_kurtaj@yahoo.com>
2023-03-23 19:05:54 +0100

    added search by criteria and source

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -6,21 +6,16 @@ import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Navbar from "../components/Nav";
-import axios from "axios";
 import Button from "@mui/material/Button";
-
-import Stack from "@mui/material/Stack";
-import IconButton from "@mui/material/IconButton";
-
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateField } from "@mui/x-date-pickers/DateField";
 import dayjs from "dayjs";
-
 import SearchIcon from "@mui/icons-material/Search";
 import Divider from "@mui/material/Divider";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
+import Nav from "../components/Navbar/Navbar";
 
 import LoadingSpinner from "../components/LoadingSpinner";
 import NewsGrid from "../components/NewsGrid";
@@ -45,38 +40,28 @@ const theme = createTheme();
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const [articles, setArticles] = useState([]);
-  const [value, setValue] = React.useState("");
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setIsLoading(true);
-  //     try {
-  //       const response = await axios.get("http://localhost:8000/api/news", {
-  //         headers: {
-  //           Authorization: `Bearer ${localStorage.getItem("token")}`,
-  //         },
-  //       });
-  //       setIsLoading(false);
-  //       setArticles(response.data.articles);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
+  const [dateValue, setDateValue] = React.useState(dayjs(new Date()));
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const [criteria, setCriteria] = useState("");
 
   const handleSubmit = async (event) => {
     setIsLoading(true);
     event.preventDefault();
     const form = new FormData(event.currentTarget);
-
     const payload = {
       criteria: form.get("keyword"),
-      fromDate: form.get("date"),
       source: form.get("source"),
       category: form.get("category"),
     };
+
+    const isEmpty = Object.values(payload).every((x) => x === null || x === "");
+    if (isEmpty) {
+      alert("OBJ is empty");
+      setIsLoading(false);
+      return;
+    }
+    payload.fromDate = form.get("date");
 
     const response = await API.get("news", { params: payload });
     setArticles(response.data.articles);
@@ -105,6 +90,8 @@ export default function Home() {
                   label="keyword"
                   type="text"
                   id="keyword"
+                  value={criteria}
+                  onChange={(event) => setCriteria(event.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -114,6 +101,9 @@ export default function Home() {
                   label="source"
                   type="text"
                   id="source"
+                  value={source}
+                  onChange={(event) => setSource(event.target.value)}
+                  disabled={category.length > 0}
                 />
               </Grid>
 
@@ -124,6 +114,9 @@ export default function Home() {
                   label="category"
                   type="text"
                   id="category"
+                  value={category}
+                  onChange={(event) => setCategory(event.target.value)}
+                  disabled={source.length > 0}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -132,8 +125,8 @@ export default function Home() {
                     label="From Date"
                     name="date"
                     id="date"
-                    value={value}
-                    onChange={(newValue) => setValue(newValue)}
+                    value={dateValue}
+                    onChange={(newValue) => setDateValue(newValue)}
                     format="YYYY-MM-DD"
                   />
                 </LocalizationProvider>
