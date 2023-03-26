381cc1cefb69c91e77e8e021b1dfcb6df34407ed
XhinoKurtaj <xhino_kurtaj@yahoo.com>
2023-03-26 19:05:10 +0200

    removed comment part in the guardian article card

--- src/components/TheGuardian/articleCard.js
+++ src/components/TheGuardian/articleCard.js
@@ -39,36 +39,5 @@ export default function ArticleCard(props) {
         </div>
       </CardActionArea>
     </Card>
-
-    // <Card className="root">
-    //   <CardActionArea onClick={goToArticle}>
-    //     <CardMedia
-    //       component="img"
-    //       height="140"
-    //       image={
-    //         "https://1000logos.net/wp-content/uploads/2021/03/The-Guardian-logo-500x325.jpg"
-    //       }
-    //       alt="the guardians alt"
-    //     />
-    //     <CardContent className="content">
-    //       <Typography
-    //         gutterBottom
-    //         variant="h5"
-    //         component="h2"
-    //         className="cart-title"
-    //       >
-    //         {article.webTitle}
-    //       </Typography>
-    //     </CardContent>
-    //     <div className="footer">
-    //       <Typography variant="body2" color="textSecondary" component="p">
-    //         {article.sectionName}
-    //       </Typography>
-    //       <Typography variant="body2" color="textSecondary" component="p">
-    //         {new Date(article.webPublicationDate).toDateString()}
-    //       </Typography>
-    //     </div>
-    //   </CardActionArea>
-    // </Card>
   );
 }
